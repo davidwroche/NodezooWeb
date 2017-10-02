e42cae2d3db5e7ba9ca5b465eb110a7b444cad38
davidwroche <davidwroche@yahoo.co.uk>
2017-10-02 15:52:29 +0100

    Info page styled

--- web.js
+++ web.js
@@ -3,7 +3,7 @@
 var hapi       = require('hapi')
 var vision     = require('vision')
 var Inert      = require('inert')
-//var handlebars = require('handlebars')
+var handlebars = require('handlebars')
 
 
 module.exports = function init_hapi(options) {
@@ -12,7 +12,7 @@ module.exports = function init_hapi(options) {
   var server = new hapi.Server()
   server.connection({port: options.port || 8000})
 
-  //server.register( vision )
+  server.register( vision )
   server.register( Inert )
 
   var seneca = options.seneca
@@ -34,6 +34,39 @@ module.exports = function init_hapi(options) {
       }
     }
   })
+
+
+  server.views({
+    engines: { html: handlebars },
+    path: folder + '/www',
+    layout: true
+  })
+
+  server.route({
+    method: 'GET', path: '/info/{mod}',
+    handler: function( request, reply )
+    {
+      //server.
+        seneca.act(
+        {
+          role: 'info',
+          cmd: 'get',
+          name: request.params.mod
+        },
+        function (err, mod) {
+          if( err ) {
+            mod = {}
+          }
+
+          mod.no_npm = !mod.npm
+          mod.no_github = !mod.github
+
+          reply.view('info',{
+            title: 'nodezoo - '+request.params.mod,
+            mod: mod
+          })
+        })
+    }})
 
   server.start()
 
